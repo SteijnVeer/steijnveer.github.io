f850f2ab152d9982cb98117de404714f5d387cae
Steijn Veer <steijnveer2006@outlook.com>
2025-04-30 16:23:46 +0200

    changed panorama.js

--- js/panorama.js
+++ js/panorama.js
@@ -2,17 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const panorama = document.getElementById('panorama');
   const slides = panorama.querySelectorAll('img');
   
-  const observer = new IntersectionObserver((entries) => {
+  const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        slides.forEach(slide => {
-          slide.style.animationPlayState = 'running';
-        });
-      } else {
-        slides.forEach(slide => {
-          slide.style.animationPlayState = 'paused';
-        });
-      }
+      slides.forEach(entry.isIntersecting?
+        slide => { slide.style.animationPlayState = 'running'; }:
+        slide => { slide.style.animationPlayState = 'paused'; }
+      );
     });
   }, {
     threshold: 0.1
